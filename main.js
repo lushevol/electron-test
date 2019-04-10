e0d91bb856c5c8914e4b4d4fe249ffb940352913
lushuai <lushevol@outlook.com>
2019-04-10 21:42:11 +0800

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {   
+  // 创建浏览器窗口
+  win = new BrowserWindow({ width: 800, height: 600, icon: './resource/favicon.ico' , autoHideMenuBar: true })
+
+  // 然后加载应用的 index.html。
+  win.loadFile('./resource/index.html')
+}
+
+app.on('ready', createWindow)
